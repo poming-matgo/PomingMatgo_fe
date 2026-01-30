d2fa422ba31fad5d6655ef04e3af10f6d17e4eb0
Hyeonwoo No <nhwgit@gmail.com>
2026-01-30 18:21:44 +0900

    Feat: 통신 type 추가

---
+++ src/types/websocket.ts
@@ -0,0 +1,25 @@
+export const Player = {
+  PLAYER_NOTHING: 'PLAYER_NOTHING',
+  PLAYER_1: 'PLAYER_1',
+  PLAYER_2: 'PLAYER_2'
+} as const;
+
+export type Player = typeof Player[keyof typeof Player];
+
+export const PlayerNumber = {
+  PLAYER_NOTHING: 0,
+  PLAYER_1: 1,
+  PLAYER_2: 2
+} as const;
+
+export interface RequestEvent<T = unknown> {
+  eventType: string;
+  data?: T;
+}
+
+export interface WebSocketResponse<T = unknown> {
+  player: Player;
+  status: string;
+  message: string;
+  data?: T;
+}
